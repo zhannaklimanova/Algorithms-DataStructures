c3c95c4fa213c08b242f6c1f66ed6938988d1056
ZhannaKlimanova <zklimanova@yahoo.com>
2020-06-20 23:31:12 -0400

    abstract class reservation add

--- Assignment1/src/assignment1/Classes/Reservation.java
+++ Assignment1/src/assignment1/Classes/Reservation.java
@@ -1,5 +1,18 @@
 package assignment1;
 
-public class Reservation {
-
+abstract class Reservation {
+	
+	String name; 
+	
+	public Reservation(String name) {
+		this.name = name;
+	}
+	
+	public final String reservationName() {
+		return this.name;
+	}
+	
+	public abstract int getCost(); 
+	
+	public abstract boolean equals(Object object);
 }
