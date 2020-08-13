010aa95c146881db6cb88e3fbb6e7c68059f9075
ZhannaKlimanova <zklimanova@yahoo.com>
2020-08-13 03:42:33 -0700

    completed more methods in assignment 3

--- Assignment3/Classes/CatTree.java
+++ Assignment3/Classes/CatTree.java
@@ -1,3 +1,4 @@
+package assignment3;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.NoSuchElementException; 
@@ -13,7 +14,6 @@ public class CatTree implements Iterable<CatInfo>{
     private CatTree(CatNode c) {
         this.root = c;
     }
-    
     
     public void addCat(CatInfo c)
     {
@@ -46,13 +46,18 @@ public class CatTree implements Iterable<CatInfo>{
         return root.costPlanning(nbMonths);
     }
     
-    
-    
     public Iterator<CatInfo> iterator()
     {
         return new CatTreeIterator();
     }
     
+
+    private void inOrderTraversal(CatNode currentNode, ArrayList<CatInfo> list) { 
+        if (currentNode.senior != null) inOrderTraversal(currentNode.senior, list);
+        if (currentNode.same != null) inOrderTraversal(currentNode.same, list);
+        list.add(currentNode.data);
+        if (currentNode.junior != null) inOrderTraversal(currentNode.junior, list);
+    }
     
     class CatNode {
         
@@ -87,37 +92,191 @@ public class CatTree implements Iterable<CatInfo>{
         
         
         public CatNode addCat(CatNode c) {
-            // ADD YOUR CODE HERE
-            return null; // DON'T FORGET TO MODIFY THE RETURN IF NEED BE
+            // case where cat to add "c" is more senior
+            if (c.data.monthHired < this.data.monthHired) {
+                if (this.senior == null) {
+                    this.senior = c;
+                    return this;
+                }
+                this.senior = this.senior.addCat(c);
+            }
+            // case where cat to add "c" is more junior
+            else if (c.data.monthHired > this.data.monthHired) {
+                if (this.junior == null) {
+                    this.junior = c;
+                    return this;
+                }
+                this.junior = this.junior.addCat(c);
+
+            }
+            // case where cat to add "c" is equal to root; must be compared based on fur thickness
+            else if (c.data.monthHired == this.data.monthHired) {
+                if (c.data.furThickness > this.data.furThickness) { // cat is more fluffy than the current one
+                    c.senior = this.senior;
+                    c.junior = this.junior;
+                    c.same = this;
+                    this.senior = null;
+                    this.junior = null;
+                    return c;
+                }
+                else if ((c.data.furThickness < this.data.furThickness)) {
+                    if (this.same == null) {
+                        this.same = c;
+                        return this;
+                    }
+                    this.same = this.same.addCat(c);
+                }
+            }
+            else { // root is null; tree is empty therefore nothing can be added
+                return null;
+            }
+            return this;
         }
         
         
-        public CatNode removeCat(CatInfo c) {
-            // ADD YOUR CODE HERE
-            return null; // DON'T FORGET TO MODIFY THE RETURN IF NEED BE
+        public CatNode removeCat(CatInfo c) { 
+            // cat to remove has been found
+            if (this.data.equals(c)) {
+                // case where c.same == null and c.senior == null and c.junior != null: c.junior replaces c
+                if ((this.same == null) && (this.senior == null) && (this.junior != null)) {
+                    this.same = this.junior.same;
+                    this.senior = this.junior.senior;
+                    this.data = this.junior.data;
+                    this.junior = this.junior.junior;
+                    return this;
+                }
+                // case where c.same == null and c.senior != null: c.senior replaces c
+                else if ((this.same == null) && (this.senior != null)) {
+                    if (this.senior.junior != null) {
+                        this.senior.junior.junior = this.junior;
+                        this.junior = this.senior.junior;
+                    }
+                    else {
+                        this.senior.junior = this.junior;
+                    }
+                    this.same = this.senior.same;
+                    this.data = this.senior.data;
+                    this.senior = this.senior.senior;
+                    return this;
+                }
+                // case where c.same != null: c.same replaces c
+                else {
+                    this.same.senior = this.senior;
+                    this.same.junior = this.junior;
+                    this.data = this.same.data;
+                    this.same = this.same.same;
+                    return this;
+                }
+            }
+            // traversing tree to find node
+            // cat is more senior
+            else if ((c.monthHired < this.data.monthHired) && (this.senior != null)) {
+                this.senior = this.senior.removeCat(c);
+            } 
+            else if ((c.monthHired > this.data.monthHired) && (this.junior != null)) {
+                this.junior = this.junior.removeCat(c);
+            }
+            else if ((c.monthHired == this.data.monthHired) && (c.furThickness < this.data.furThickness) && (this.same != null)) {
+                this.same = this.same.removeCat(c);
+            }
+            // case where there's nothing to remove
+            // root == null
+            else if (root == null){
+                return null;
+            }
+            return this; 
         }
-        
         
         public int mostSenior() {
-            // ADD YOUR CODE HERE
-            return -1; //CHANGE THIS
+            try {
+                if (this.senior == null) {
+                    return this.data.monthHired;
+                }
+                else {
+                    return this.senior.mostSenior();
+                }
+            }
+            catch (Exception e) {
+                System.out.println("Check your tree, perhaps it is empty!");
+            }
+            return this.senior.data.monthHired; 
         }
         
-        public int fluffiest() {
-            // ADD YOUR CODE HERE
-            return -1; // DON'T FORGET TO MODIFY THE RETURN IF NEED BE
+        public int fluffiest() { 
+            int fluffiestCat = this.data.furThickness; 
+            
+            if (this.senior == null && this.junior == null) {
+                return this.data.furThickness;
+            }
+            else if (this.senior != null) {
+                fluffiestCat = Math.max(fluffiestCat, this.senior.fluffiest());
+            }
+            else if (this.junior != null) {
+                fluffiestCat = Math.max(fluffiestCat, this.junior.fluffiest());
+            }
+            else {
+                return -1;
+            }
+            return fluffiestCat;
         }
+       
         
         
         public int hiredFromMonths(int monthMin, int monthMax) {
-            // ADD YOUR CODE HERE
-            return -1; // DON'T FORGET TO MODIFY THE RETURN IF NEED BE
+            int count = 0;
+            try {
+                ArrayList<CatInfo> cats = new ArrayList<CatInfo>();
+                inOrderTraversal(root, cats);
+                int start = binarySearch(cats, monthMin);
+                    
+                for (int i = start; i < cats.size(); i++) {
+                    if ((cats.get(i).monthHired >= monthMin) && (cats.get(i).monthHired <= monthMax)) {
+                            count++;
+                    }
+                }
+                return count;
+            }
+            catch (Exception e) {
+                System.out.println("Element not");
+            }
+            return count;
+        } 
+        
+        private int binarySearch(ArrayList<CatInfo> list, int elementToFind) {
+            int left = 0;
+            int right = list.size()-1;
             
+            while(left <= right) {
+                int middle = (left+right)/2;
+                if (list.get(middle).monthHired == elementToFind) {
+                    return middle;
+                }
+                else {
+                    if (elementToFind < list.get(middle).monthHired) {
+                        right = middle-1;
+                    }
+                    else {
+                        left = middle+1;
+                    }
+                }
+            }
+            return -1; // if elementToFind is not in list
         }
         
+        
         public CatInfo fluffiestFromMonth(int month) {
-            // ADD YOUR CODE HERE
-            return null; // DON'T FORGET TO MODIFY THE RETURN IF NEED BE
+            if (month == this.data.monthHired) {
+                return this.data;
+            }
+            else if (month < this.data.monthHired && this.senior != null) {
+                return this.senior.fluffiestFromMonth(month);
+            }
+            else if (month > this.data.monthHired && this.junior != null) {
+                return this.junior.fluffiestFromMonth(month);
+            } 
+            else {
+                return null;
+            }
         }
         
         public int[] costPlanning(int nbMonths) {
@@ -128,10 +287,9 @@ public class CatTree implements Iterable<CatInfo>{
     }
     
     private class CatTreeIterator implements Iterator<CatInfo> {
-        // HERE YOU CAN ADD THE FIELDS YOU NEED
-        
+       
         public CatTreeIterator() {
-            //YOUR CODE GOES HERE
+
         }
         
         public CatInfo next(){
--- Assignment3/Testers/MiniTester.java
+++ Assignment3/Testers/MiniTester.java
@@ -1,5 +1,9 @@
+package assignment3;
+
 import java.util.Iterator;
 import java.util.NoSuchElementException;
+
+import assignment3.CatTree.CatNode;
 
 public class MiniTester 
 {	
@@ -211,6 +215,54 @@ public class MiniTester
 
 		return grade;
 	}
+	
+	private int test_addCat_6(int testIdx) 
+	{
+		String comment = "[" + testIdx + "]: Test addCat where cat to add has same seniority to c and is more fluffy.\n";
+		int maxScore = 2;
+		int grade = 0;
+
+		try {
+			CatInfo a = new CatInfo("A", 87, 50, 243, 40);
+			CatInfo f = new CatInfo("F", 85, 60, 245, 20);
+			CatInfo b = new CatInfo("B", 88, 60, 245, 20);
+			CatInfo d = new CatInfo("D", 85, 50, 245, 20);
+			CatInfo e = new CatInfo("E", 85, 45, 245, 20);
+			CatInfo h = new CatInfo("H", 95, 55, 245, 20);
+			CatInfo z = new CatInfo("Z", 85, 65, 245, 20);
+			CatInfo x = new CatInfo("X", 87, 60, 244, 20);
+			
+			CatTree t = new CatTree(a);
+			t.addCat(f);
+			t.addCat(b);
+			t.addCat(d);
+			t.addCat(e);
+			t.addCat(h);
+			t.addCat(z);
+			t.addCat(x);
+
+			if (t.root.senior.data.equals(z) && t.root.senior.same.data.equals(f)) {
+				grade += 2;
+			} else {
+				comment = comment + "Error: structure of tree is wrong when adding a cat with the same seniority that is less fluffy.\n";
+			}
+			if (t.root.data.equals(x) && t.root.senior.data.equals(z) && t.root.same.data.equals(a) && t.root.junior.data.equals(b)) {
+				grade += 2;
+			} else {
+				comment = comment + "Error: structure of tree is wrong when adding a cat with the same seniority that is less fluffy.\n";
+			}
+
+			write(gradeString(grade, maxScore, comment));
+
+
+		} catch (Exception e) {
+			comment = comment + "Exception Found: " + e.toString() + "\n";
+			e.printStackTrace();
+			write(gradeString(0, maxScore, comment));
+		}
+
+		return grade;
+	}
 
 
 	//----------------------------------------------------------------------------------------------------------------------
@@ -297,9 +349,120 @@ public class MiniTester
 
 		return grade;
 	}
+	
+	private int test_removeCat_3(int testIdx) 
+	{
+		String comment = "[" + testIdx + "]: Test removeCat when Node to remove is at root.\n";
+		int maxScore = 4;
+		int grade = 0;
 
-	//----------------------------------------------------------------------------------------------------------------------
-	//MOST SENIOR
+		try {
+
+			/*
+			1. Node not in the tree		1
+			2. at root (simple case)	3
+			*/
+			CatInfo a = new CatInfo("A", 87, 50, 243, 40);
+			CatInfo b = new CatInfo("B", 85, 60, 240, 30);
+			CatInfo c = new CatInfo("C", 88, 70, 248, 10);
+			
+			CatTree t = new CatTree(a);
+			t.addCat(c);
+			
+			t.removeCat(a);	
+			
+			if (t.root.data.equals(c) && t.root.senior == null && t.root.same == null && t.root.junior == null) {
+				grade += 1;
+			} else {
+				comment = comment + "Error: removeCat changes the structure of the tree in some way when the cat to remove isn't in the tree.\n";
+			}
+			CatTree q = new CatTree(a);
+			q.addCat(b);
+			q.addCat(c);
+			q.removeCat(a);
+			
+			if (q.root.data.equals(b) && q.root.same == null && q.root.senior == null && q.root.junior.data.equals(c)) {
+				grade += 3;
+			} else{
+				comment = comment + "Error: unexpected tree structure when removing the root of a tree.\n";
+			}
+
+			write(gradeString(grade, maxScore, comment));	
+
+		} catch (Exception e) {
+			comment = comment + "Exception Found: " + e.toString() + "\n";
+			e.printStackTrace();
+			write(gradeString(0, maxScore, comment));
+		}
+
+		return grade;
+	}
+	
+	private int test_removeCat_main(int testIdx) 
+	{
+		String comment = "[" + testIdx + "]: Test removeCat when multiple cases.\n";
+		int maxScore = 18;
+		int grade = 0;
+
+		try {
+			CatInfo c = new CatInfo("C", 87, 55, 243, 40);
+			CatInfo a = new CatInfo("A", 87, 50, 243, 40);
+			CatInfo f = new CatInfo("F", 85, 60, 245, 20);
+			CatInfo b = new CatInfo("B", 88, 60, 245, 20);
+			CatInfo d = new CatInfo("D", 85, 50, 245, 20);
+			CatInfo g = new CatInfo("G", 86, 55, 202, 20);
+			CatInfo e = new CatInfo("E", 85, 45, 245, 20);
+			CatInfo h = new CatInfo("H", 95, 55, 245, 20);
+//			CatInfo x = new CatInfo("X", 87, 45, 245, 20);
+			
+			CatTree t = new CatTree(c);
+			
+			t.addCat(a);
+			t.addCat(f);
+			t.addCat(b);
+			t.addCat(d);
+			t.addCat(e);
+			t.addCat(h);
+			t.addCat(g);
+//			t.addCat(x);
+			
+			t.removeCat(c);
+			
+			if (t.root.data.equals(a) && t.root.senior.data.equals(f) && t.root.junior.data.equals(b)) {
+				grade += 6;
+			} else {
+				comment = comment + "Error: unexpected tree structure when removing an internal node when same != null.\n";
+			}
+			
+			t.removeCat(a);
+			
+			if ((t.root.data.equals(f) && t.root.junior.data.equals(g) && t.root.junior.junior.data.equals(b) && t.root.same.data.equals(d))) {
+				grade += 6;
+			} else {
+				comment = comment + "Error: unexpected tree structure when removing an internal node when same == null, senior != null, junior != null.\n";
+			}
+			
+			t.removeCat(b);
+			
+			if (t.root.data.equals(f) && t.root.junior.data.equals(g) && t.root.junior.junior.data.equals(h)) {
+				grade += 6;
+			} else {
+				comment = comment + "Error: unexpected tree structure when removing an internal node when same == null, senior == null, junior != null.\n";
+			}
+			
+			write(gradeString(grade, maxScore, comment));	
+			
+		} catch (Exception e) {
+			comment = comment + "Exception Found: " + e.toString() + "\n";
+			e.printStackTrace();
+			write(gradeString(0, maxScore, comment));
+		}
+
+		return grade;
+	}
+
+//	//----------------------------------------------------------------------------------------------------------------------
+//	//MOST SENIOR
 
 	private int test_mostSenior_0(int testIdx) 
 	{
@@ -336,10 +499,56 @@ public class MiniTester
 
 		return grade;
 	}
+	
+	private int test_mostSenior_1(int testIdx) 
+	{
+		String comment = "[" + testIdx + "]: Test mostSenior when c.senior != null\n";
+		int maxScore = 4;
+		int grade = 0;
+
+		try {
+			
+			CatInfo c = new CatInfo("C", 87, 55, 243, 40);
+			CatInfo a = new CatInfo("A", 87, 50, 243, 40);
+			CatInfo f = new CatInfo("F", 85, 60, 245, 20);
+			CatInfo b = new CatInfo("B", 88, 60, 245, 20);
+			CatInfo d = new CatInfo("D", 85, 50, 245, 20);
+			CatInfo g = new CatInfo("G", 86, 55, 202, 20);
+			CatInfo e = new CatInfo("E", 85, 45, 245, 20);
+			CatInfo h = new CatInfo("H", 95, 55, 245, 20);
+			CatInfo x = new CatInfo("X", 84, 55, 245, 20);
+			
+			CatTree t = new CatTree(c);
+			t.addCat(a);
+			t.addCat(f);
+			t.addCat(b);
+			t.addCat(d);
+			t.addCat(e);
+			t.addCat(h);
+			t.addCat(g);
+			t.addCat(x);
+
+			int most = t.mostSenior();
+			if (most == 84) {
+				grade += 4;
+			} else {
+				comment = comment + "Error: MostSenior is incorrect when c.senior != null.\n";
+			}
+			
+			write(gradeString(grade, maxScore, comment));	
+
+		} catch (Exception e) {
+			comment = comment + "Exception Found: " + e.toString() + "\n";
+			e.printStackTrace();
+			write(gradeString(0, maxScore, comment));
+		}
+
+		return grade;
+	}
 
 
-	//----------------------------------------------------------------------------------------------------------------------
-	//FLUFFIEST
+//	//----------------------------------------------------------------------------------------------------------------------
+//	//FLUFFIEST
 
 	private int test_fluffiest_0(int testIdx) 
 	{
@@ -385,10 +594,63 @@ public class MiniTester
 
 		return grade;
 	}
+	
+	private int test_fluffiest_1(int testIdx) 
+	{
+		String comment = "[" + testIdx + "]: Test fluffiest when root is fluffiest and when not root is fluffiest.\n";
+		int maxScore = 3;
+		int grade = 0;
+
+		try {
+			CatInfo c = new CatInfo("C", 87, 55, 243, 40);
+			CatInfo a = new CatInfo("A", 87, 50, 243, 40);
+			CatInfo f = new CatInfo("F", 85, 60, 245, 20);
+			CatInfo b = new CatInfo("B", 88, 60, 245, 20);
+			CatInfo d = new CatInfo("D", 85, 50, 245, 20);
+			CatInfo g = new CatInfo("G", 86, 55, 202, 20);
+			CatInfo e = new CatInfo("E", 85, 45, 245, 20);
+			CatInfo h = new CatInfo("H", 95, 55, 245, 20);
+			
+			CatTree t = new CatTree(c);
+			t.addCat(a);
+			t.addCat(f);
+			t.addCat(b);
+			t.addCat(d);
+			t.addCat(e);
+			t.addCat(h);
+			t.addCat(g);
+			
+			int most = t.root.fluffiest();
+			if (most == 60) {
+				grade += 3;
+			} else {
+				comment = comment + "Error: fluffiest is incorrect when the root is the fluffiest.";
+			}
+			
+			
+			int most2 = t.root.senior.same.fluffiest();
+			if (most2 == 50) {
+				grade += 3;
+			} else {
+				comment = comment + "Error: fluffiest is incorrect when the root is the fluffiest.";
+			}
+			
+			write(gradeString(grade, maxScore, comment));	
 
 
-	//----------------------------------------------------------------------------------------------------------------------
-	//HIREDFORMONTHS
+
+		} catch (Exception e) {
+			comment = comment + "Exception Found: " + e.toString() + "\n";
+			e.printStackTrace();
+			write(gradeString(0, maxScore, comment));
+		}
+
+		return grade;
+	}
+
+
+//	//----------------------------------------------------------------------------------------------------------------------
+//	//HIREDFORMONTHS
 	private int test_hired_0(int testIdx) 
 	{
 		String comment = "[" + testIdx + "]: Test hiredFromMonths when Cat is in c.junior.\n";
@@ -487,7 +749,105 @@ public class MiniTester
 		return grade;
 	}
 	
-	//----------------------------------------------------------------------------------------------------------------------
+	private int test_hired_2(int testIdx) 
+	{
+		String comment = "[" + testIdx + "]: Test hiredFromMonths when Cats are spread over several branches \n";
+		int maxScore = 2;
+		int grade = 0;
+
+		try {
+			CatInfo a = new CatInfo("A", 100, 40, 243, 40);
+			CatInfo b = new CatInfo("B", 50, 60, 240, 30);
+			CatInfo c = new CatInfo("C", 125, 75, 248, 10);
+			CatInfo d = new CatInfo("D", 100, 15, 245, 50);
+			CatInfo e = new CatInfo("E", 40, 25, 245, 20);
+			CatInfo f = new CatInfo("F", 50, 35, 247, 15);
+			CatInfo g = new CatInfo("G", 75, 40, 247, 27);
+			CatInfo h = new CatInfo("H", 100, 50, 247, 27);
+			CatInfo i = new CatInfo("I", 110, 65, 247, 100);
+			CatInfo j = new CatInfo("J", 125, 75, 247, 100);
+			CatInfo k = new CatInfo("K", 130, 15, 247, 100);
+						
+			CatTree t = new CatTree(a);
+			t.addCat(b);
+			t.addCat(c);
+			t.addCat(d);
+			t.addCat(e);
+			t.addCat(f);
+			t.addCat(g);
+			t.addCat(h);
+			t.addCat(i);
+			t.addCat(j);
+			t.addCat(k);
+			
+			int num = t.root.hiredFromMonths(40, 135);
+			if (num == 10) {
+				grade += 2;
+			} else {
+				comment = comment + "Error: hiredFromMonths when Cat is in same branch.\n";
+			}
+			
+			write(gradeString(grade, maxScore, comment));	
+
+		} catch (Exception e) {
+			comment = comment + "Exception Found: " + e.toString() + "\n";
+			e.printStackTrace();
+			write(gradeString(0, maxScore, comment));
+		}
+
+		return grade;
+	}
+	
+	private int test_hired_3(int testIdx) 
+	{
+		String comment = "[" + testIdx + "]: Test hiredFromMonths when Cat is not in list \n";
+		int maxScore = 2;
+		int grade = 0;
+
+		try {
+			CatInfo a = new CatInfo("A", 100, 40, 243, 40);
+			CatInfo b = new CatInfo("B", 50, 60, 240, 30);
+			CatInfo c = new CatInfo("C", 125, 75, 248, 10);
+			CatInfo d = new CatInfo("D", 100, 15, 245, 50);
+			CatInfo e = new CatInfo("E", 40, 25, 245, 20);
+			CatInfo f = new CatInfo("F", 50, 35, 247, 15);
+			CatInfo g = new CatInfo("G", 75, 40, 247, 27);
+			CatInfo h = new CatInfo("H", 100, 50, 247, 27);
+			CatInfo i = new CatInfo("I", 110, 65, 247, 100);
+			CatInfo j = new CatInfo("J", 125, 75, 247, 100);
+			CatInfo k = new CatInfo("K", 130, 15, 247, 100);
+						
+			CatTree t = new CatTree(a);
+			t.addCat(b);
+			t.addCat(c);
+			t.addCat(d);
+			t.addCat(e);
+			t.addCat(f);
+			t.addCat(g);
+			t.addCat(h);
+			t.addCat(i);
+			t.addCat(j);
+			t.addCat(k);
+			
+			int num = t.root.hiredFromMonths(30, 30);
+			if (num == 0) {
+				grade += 2;
+			} else {
+				comment = comment + "Error: hiredFromMonths when Cat is in same branch.\n";
+			}
+			
+			write(gradeString(grade, maxScore, comment));	
+
+		} catch (Exception e) {
+			comment = comment + "Exception Found: " + e.toString() + "\n";
+			e.printStackTrace();
+			write(gradeString(0, maxScore, comment));
+		}
+
+		return grade;
+	}
+	
+//	//----------------------------------------------------------------------------------------------------------------------
 	//FLUFFIESTFROMMONTH
 	private int test_fluffyFrom_0(int testIdx) 
 	{
@@ -519,7 +879,7 @@ public class MiniTester
 			t.addCat(i);
 			t.addCat(j);
 			t.addCat(k);
-			
+				
 			CatInfo ret = t.root.fluffiestFromMonth(100);
 			if (ret.equals(h)) {
 				grade += 2;
@@ -538,43 +898,44 @@ public class MiniTester
 		return grade;
 	}
 	
-	//----------------------------------------------------------------------------------------------------------------------
-	//COSTPLANNING
-	private int test_costPlan_0(int testIdx) 
-	{
-		String comment = "[" + testIdx + "]: Test costPlanning - test 0.\n";
-		int maxScore = 2;
-		int grade = 0;
-
-		try {
-			CatInfo a = new CatInfo("A", 100, 40, 243, 20);
-			CatInfo c = new CatInfo("C", 125, 75, 248, 20);
-			CatInfo d = new CatInfo("D", 100, 15, 245, 20);
-									
-			CatTree t = new CatTree(a);
-			t.addCat(c);
-			t.addCat(d);
-						
-			int []plan = t.root.costPlanning(75);
-			if (plan.length == 75) {
-				grade += 2;
-			} else {
-				comment = comment + "Error: costPlanning did not return an array of required size.\n";
-			}
-						
-			write(gradeString(grade, maxScore, comment));	
-
-		} catch (Exception e) {
-			comment = comment + "Exception Found: " + e.toString() + "\n";
-			e.printStackTrace();
-			write(gradeString(0, maxScore, comment));
-		}
-
-		return grade;
-	}
+//	//----------------------------------------------------------------------------------------------------------------------
+//	//COSTPLANNING
+//	private int test_costPlan_0(int testIdx) 
+//	{
+//		String comment = "[" + testIdx + "]: Test costPlanning - test 0.\n";
+//		int maxScore = 2;
+//		int grade = 0;
+//
+//		try {
+//			CatInfo a = new CatInfo("A", 100, 40, 243, 20);
+//			CatInfo c = new CatInfo("C", 125, 75, 248, 20);
+//			CatInfo d = new CatInfo("D", 100, 15, 245, 20);
+//									
+//			CatTree t = new CatTree(a);
+//			t.addCat(c);
+//			t.addCat(d);
+//						
+//			int []plan = t.root.costPlanning(75);
+//			if (plan.length == 75) {
+//				grade += 2;
+//			} else {
+//				comment = comment + "Error: costPlanning did not return an array of required size.\n";
+//			}
+//						
+//			write(gradeString(grade, maxScore, comment));	
+//
+//		} catch (Exception e) {
+//			comment = comment + "Exception Found: " + e.toString() + "\n";
+//			e.printStackTrace();
+//			write(gradeString(0, maxScore, comment));
+//		}
+//
+//		return grade;
+//	}
 
 	public static void main(String[] args) 
 	{
+		
 		MiniTester m = new MiniTester();
 		int total = 0;
 
@@ -585,27 +946,35 @@ public class MiniTester
 		total += m.test_addCat_3(3);
 		total += m.test_addCat_4(4);
 		total += m.test_addCat_5(5);
+		total += m.test_addCat_6(6);
 
 		//REMOVE CAT
 		total += m.test_removeCat_0(6);
 		total += m.test_removeCat_1(7);
+		total += m.test_removeCat_3(7);		
+		total += m.test_removeCat_main(7);
 
-		//MOST SENIOR
+//		//MOST SENIOR
 		total += m.test_mostSenior_0(8);
-
-		//FLUFFIEST
+		total += m.test_mostSenior_1(8);
+		
+//		//FLUFFIEST
 		total += m.test_fluffiest_0(9);
+		total += m.test_fluffiest_1(9);
 
-		//HIRED FROM MONTHS
+
+//		//HIRED FROM MONTHS
 		total += m.test_hired_0(10);
 		total += m.test_hired_1(11);
-
-		//FLUFFIEST FROM MONTH
+		total += m.test_hired_2(12);
+		total += m.test_hired_3(13);
+//
+//		//FLUFFIEST FROM MONTH
 		total += m.test_fluffyFrom_0(12);
-
-		//COST PLANNING
-		total += m.test_costPlan_0(13);
-		
-		//m.write(m.gradeString(total, 50, "Your Final Tester "));
+//
+//		//COST PLANNING
+//		total += m.test_costPlan_0(13);
+//		
+//		//m.write(m.gradeString(total, 50, "Your Final Tester "));
 	}
 }
